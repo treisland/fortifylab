ff0337766816139c64d4ea18479adc75cfbaff3a
treisland <t.island84@gmail.com>
2026-07-31 19:41:38 +0000

    docs: add first successful scan walkthrough

---
+++ docs/examples/sast/SyntheticGreeting.java
@@ -0,0 +1,7 @@
+public final class SyntheticGreeting {
+    private SyntheticGreeting() {}
+
+    public static void main(String[] args) {
+        System.out.println("Fortify Lab synthetic sample");
+    }
+}
